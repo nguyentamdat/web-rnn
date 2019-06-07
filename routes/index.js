86a3f807a89994a1014de038d0d6eee588bb9ac8
nguyentamdat <nguyentamdat@gmail.com>
2019-06-07 14:25:03 +0700

    Add mock data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Express",
+    arrayOfDevices: [
+      { id: 1, status: false },
+      { id: 2, status: true },
+      { id: 3, status: false },
+      { id: 4, status: true }
+    ]
+  });
 });
 
 module.exports = router;
