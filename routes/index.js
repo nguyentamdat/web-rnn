29cfa967fc0f78a4a98bef77225dd86d0cd7c4d9
nguyentamdat <nguyentamdat@gmail.com>
2019-06-08 15:29:03 +0700

    Add mqtt communicate

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", {
-    title: "Express",
-    arrayOfDevices: [
-      { id: 1, status: false },
-      { id: 2, status: true },
-      { id: 3, status: false },
-      { id: 4, status: true }
-    ]
-  });
+  res.render("index");
 });
 
 module.exports = router;
